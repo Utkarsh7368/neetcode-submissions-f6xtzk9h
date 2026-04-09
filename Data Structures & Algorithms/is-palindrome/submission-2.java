a5ff45ba506150503151beca9c0cbcbd4381c09d
Utkarsh Gupta <121254368+Utkarsh7368@users.noreply.github.com>
2026-04-09 15:47:52 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            if(Character.isLetterOrDigit(s.charAt(i))){
+                sb.append(s.charAt(i));
+            }
+        }
+        System.out.println(sb.toString());
+        int n=sb.length();
+        String str=sb.toString();
+        for(int i=0;i<n/2;i++){
+           if(Character.toLowerCase(str.charAt(i)) != 
+   Character.toLowerCase(str.charAt(n - i - 1))){
+    return false;
+}
+        }
+    return true;
+    }
+}
